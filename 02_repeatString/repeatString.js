4f28088cd1f392e67efef49f514d591388aa301c
Peter Oodo <pifeanyi2010@gmail.com>
2022-08-30 18:59:24 +0100

    I completed the assignment on The Odin Project

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string,num) {
+ if (num > 0) {
+  return string.repeat(num);
+ }
+if (num === 0){
+ return ''
+}
+ else{
+  return 'ERROR'
+ }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
