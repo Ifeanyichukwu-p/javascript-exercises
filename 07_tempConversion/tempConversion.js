1b9d1b7a5661ed759059640a3557cdf3c28c8993
Peter Oodo <pifeanyi2010@gmail.com>
2022-09-10 22:37:53 +0100

    I did other assignments from The Odin Project

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  let fTemp = fahrenheit ;
+  let cTemp = (fTemp - 32)*5/9;
+  let celcius = Math.round(cTemp*10)/10;
+  return celcius;
 
 };
 
-const ctof = function() {
+const ctof = function(celcius) {
+  let cTemp = celcius;
+  let fTemp = (9*cTemp )*1/5 +32;
+  let fahrenheit = Math.round(fTemp*10)/10;
+  return fahrenheit;
 
 };
 
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,52 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
+
+};
+
+const subtract = function(a,b) {
+	
+  
+  return a - b;
+};
+
+const sum = function(array) {
+  let sum = 0; 
+
+
+  array.forEach(item => {
+    sum += item;
+  });
+  return sum;
+}
+
+const multiply = function(array) {
+
+  let total = 1; 
+
+
+  array.forEach(item => {
+    total *= item;
+  });
+return total;
+};
+
+
+
+const power = function(a,b) {
+  return Math.pow(a,b);
 	
 };
 
-const subtract = function() {
-	
+const factorial = function(n) {
+
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
